df44a4346461f4c808b807b4ef2874afe4b63726
Ian Renton <ian@ianrenton.com>
2023-03-12 11:38:04 +0000

    Finish coding

--- udp_servo_control.c
+++ udp_servo_control.c
@@ -34,16 +34,34 @@
 #include <rc/time.h>
 #include <rc/adc.h>
 #include <rc/servo.h>
+#include <pthread.h>
 
 // Set the port on which to listen for UDP packets.
 #define UDP_PORT 2031
 // Set which servo outputs are used
 #define THROTTLE_SERVO 0
 #define RUDDER_SERVO 1
+// Set the microsecond pulse lengths corresponding to throttle and rudder
+// positions
+#define THROTTLE_MIN_PULSE_LENGTH_USEC 900
+#define THROTTLE_MAX_PULSE_LENGTH_USEC 2100
+#define RUDDER_MIN_PULSE_LENGTH_USEC 900
+#define RUDDER_MAX_PULSE_LENGTH_USEC 2100
+// Set the pulse rate
+#define SERVO_PULSE_RATE_HZ 50
 // Set the timeout - zero the demands after this many seconds without receiving
 // a packet.
 #define COMMS_TIMEOUT_SEC 5
 
+
+// Additional calculated defines
+#define THROTTLE_RANGE_USEC (THROTTLE_MAX_PULSE_LENGTH_USEC - THROTTLE_MIN_PULSE_LENGTH_USEC)
+#define RUDDER_RANGE_USEC (RUDDER_MAX_PULSE_LENGTH_USEC - RUDDER_MIN_PULSE_LENGTH_USEC)
+#define RUDDER_CENTRE_PULSE_LENGTH_USEC ((RUDDER_MAX_PULSE_LENGTH_USEC-RUDDER_MIN_PULSE_LENGTH_USEC) / 2.0 + RUDDER_MIN_PULSE_LENGTH_USEC)
+
+// Globals and mutex for passing throttle and rudder info between threads
+double throttle, rudder = 0.0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 // interrupt handler to catch ctrl-c
 static int running = 0;
@@ -52,11 +70,77 @@ static void __signal_handler(__attribute__ ((unused)) int dummy) {
     return;
 }
 
-// Function to zero outputs
-void zero_output() {
-    printf("zero output\n");
-    // todo zero output
-    rc_usleep(2000000);
+// Comms thread. Receiving UDP packets is handled here. This is forked off from
+// the main code, which continues running to handle the servos.
+void *commsThread() {
+    // Create UDP socket, exit on failure
+    int udpsocket;
+    if ((udpsocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+        fprintf(stderr,"create socket failed\n");
+        return NULL;
+    }
+
+    // Configure socket
+    struct sockaddr_in listener;
+    bzero(&listener, sizeof(listener));
+    listener.sin_family      = AF_INET;
+    listener.sin_port        = htons(UDP_PORT);
+    listener.sin_addr.s_addr = htonl(INADDR_ANY);
+    int reuse = 1;
+    if (setsockopt(udpsocket, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+        fprintf(stderr,"configure socket failed 1\n");
+        return NULL;
+    }
+    struct timeval timeout;
+    timeout.tv_sec = COMMS_TIMEOUT_SEC;
+    timeout.tv_usec = 0;
+    if (setsockopt(udpsocket, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0) {
+        fprintf(stderr,"configure socket failed 2\n");
+        return NULL;
+    }
+
+    // Bind socket, exit on failure
+    if (bind(udpsocket, (struct sockaddr*) &listener, sizeof(listener)) < 0) {
+        fprintf(stderr,"bind socket failed\n");
+        return NULL;
+    }
+
+    // Listen indefinitely until the program is stopped
+    while (running) {
+        // Read UDP packet, timeout after 5 seconds
+        printf("Waiting for packet...\n");
+        char buffer[20] = {0};
+        struct sockaddr_in sender;
+        unsigned int senderlen = sizeof(sender);
+        bzero(&sender, sizeof(sender));
+        int nbytes = recvfrom(udpsocket, buffer, sizeof(buffer), 0, (struct sockaddr*) &sender, &senderlen);
+
+        double tmpThrottle = 0.0, tmpRudder = 0.0;
+        if (nbytes > 0) {
+            // Parse message
+            char *eptr;
+            buffer[nbytes] = '\0';
+            char delim[] = ",";
+            char *ptr = strtok(buffer, delim);
+            tmpThrottle = strtod(ptr, &eptr);
+            ptr = strtok(NULL, delim);
+            tmpRudder = strtod(ptr, &eptr);
+
+            printf("Received demand: Throttle %f Rudder %f\n", tmpThrottle, tmpRudder);
+        } else {
+            printf("No bytes received, zeroing outputs\n");
+        }
+
+        // Write to global variables to pass the new demands to the other thread
+        pthread_mutex_lock(&mutex);
+        throttle = tmpThrottle;
+        rudder = tmpRudder;
+        pthread_mutex_unlock(&mutex);
+    }
+
+    // Close the socket
+    close(udpsocket);
+    return NULL;
 }
 
 int main()  {
@@ -69,9 +153,9 @@ int main()  {
         fprintf(stderr,"ERROR: failed to run rc_adc_init()\n");
         return -1;
     }
-    if (rc_adc_batt()<6.0) {
-        fprintf(stderr,"ERROR: battery disconnected or insufficiently charged to drive servos\n");
-// todo re-enable       return -1;
+    while (rc_adc_batt()<6.0) {
+        printf("Battery disconnected or insufficiently charged to drive servos, waiting until connected...\n");
+        rc_usleep(5000000);
     }
     rc_adc_cleanup();
 
@@ -80,81 +164,58 @@ int main()  {
 
     // turn on power
     printf("Turning On 6V Servo Power Rail\n");
-// todo re-enable    rc_servo_power_rail_en(1);
+    rc_servo_power_rail_en(1);
 
     // Zero outputs at startup
-    zero_output();
+    printf("Zero output\n");
+    rc_servo_send_pulse_us(THROTTLE_SERVO, THROTTLE_MIN_PULSE_LENGTH_USEC);
+    rc_servo_send_pulse_us(RUDDER_SERVO, RUDDER_CENTRE_PULSE_LENGTH_USEC);
+    rc_usleep(2000000);
 
-    // Create UDP socket, exit on failure
-    int udpsocket;
-    if ((udpsocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
-        fprintf(stderr,"create socket failed\n");
-        return -1;
-    }
+    // Spin off a new thread for the UDP socket listening
+    pthread_t udp_socket_thread;
+    pthread_create(&udp_socket_thread, NULL, commsThread, NULL);
 
-    // Configure socket
-    struct sockaddr_in listener;
-    bzero(&listener, sizeof(listener));
-    listener.sin_family      = AF_INET;
-    listener.sin_port        = htons(UDP_PORT);
-    listener.sin_addr.s_addr = htonl(INADDR_ANY);
-    int reuse = 1;
-    if (setsockopt(udpsocket, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-        fprintf(stderr,"configure socket failed 1\n");
-        return -1;
-    }
-    struct timeval timeout;
-    timeout.tv_sec = COMMS_TIMEOUT_SEC;
-    timeout.tv_usec = 0;
-    if (setsockopt(udpsocket, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0) {
-        fprintf(stderr,"configure socket failed 2\n");
-        return -1;
-    }
-
-    // Bind socket, exit on failure
-    if (bind(udpsocket, (struct sockaddr*) &listener, sizeof(listener)) < 0) {
-        fprintf(stderr,"bind socket failed\n");
-        return -1;
-    }
-
-    // Main run loop
+    // Control servos indefinitely until the program is stopped
     while (running) {
-        // Read UDP packet, timeout after 5 seconds
-        printf("Waiting for packet...\n");
-        char buffer[20] = {0};
-        struct sockaddr_in sender;
-        unsigned int senderlen = sizeof(sender);
-        bzero(&sender, sizeof(sender));
-        int nbytes = recvfrom(udpsocket, buffer, sizeof(buffer), 0, (struct sockaddr*) &sender, &senderlen);
+        // Get global variables
+        pthread_mutex_lock(&mutex);
+        double tmpThrottle = throttle;
+        double tmpRudder = rudder;
+        pthread_mutex_unlock(&mutex);
 
-        // If timed out, zero outputs
-        if (nbytes < 0) {
-            printf("No bytes received\n");
-            zero_output();
-            continue;
+        // Calculate outputs
+        double throttle_usec = THROTTLE_MIN_PULSE_LENGTH_USEC;
+        if (tmpThrottle >= 0 && tmpThrottle <= 100.0) {
+            throttle_usec = (tmpThrottle / 100.0 * THROTTLE_RANGE_USEC) + THROTTLE_MIN_PULSE_LENGTH_USEC;
+        } else {
+            printf("Throttle demand out of range\n");
+        }
+        double rudder_usec = RUDDER_CENTRE_PULSE_LENGTH_USEC;
+        if (tmpRudder >= -100.0 && tmpRudder <= 100.0) {
+            rudder_usec = (tmpRudder / 200.0 * RUDDER_RANGE_USEC) + RUDDER_CENTRE_PULSE_LENGTH_USEC;
+        } else {
+            printf("Rudder demand out of range\n");
         }
 
-        // Parse message
-        double throttle, rudder;
-        char *eptr;
-        buffer[nbytes] = '\0';
-        char delim[] = ",";
-        char *ptr = strtok(buffer, delim);
-        throttle = strtod(ptr, &eptr);
-        ptr = strtok(NULL, delim);
-        rudder = strtod(ptr, &eptr);
-
-        printf("Throttle: %f\n", throttle);
-        printf("Rudder: %f\n", rudder);
-        
         // Set outputs
-        // todo
+        //printf("Sending servo pulse: Ch%d %f, Ch%d %f\n", THROTTLE_SERVO, throttle_usec, RUDDER_SERVO, rudder_usec);
+        rc_servo_send_pulse_us(THROTTLE_SERVO, throttle_usec);
+        rc_servo_send_pulse_us(RUDDER_SERVO, rudder_usec);
+
+        rc_usleep(1000000/SERVO_PULSE_RATE_HZ);
     }
 
-    // Turn off power rail, clean up & close sockets
+    // Wait for comms thread to finish
+    pthread_join(udp_socket_thread, NULL);
+
+    // Zero outputs
+    rc_servo_send_pulse_us(THROTTLE_SERVO, THROTTLE_MIN_PULSE_LENGTH_USEC);
+    rc_servo_send_pulse_us(RUDDER_SERVO, RUDDER_CENTRE_PULSE_LENGTH_USEC);
+
+    // Turn off power rail & clean up
     rc_usleep(50000);
     rc_servo_power_rail_en(0);
     rc_servo_cleanup();
-    close(udpsocket);
     return 0;
 }
